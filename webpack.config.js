afea002dc6344d286466c082c59ee7a963267dac
shriram <shriram.k.raja@gmail.com>
2019-08-12 19:16:39 +0530

    added axios, redux, some babel presets

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,17 @@
 const webpack = require('webpack');
 
 module.exports = {
-    entry: './app/index.js',
+    devtool: 'inline-source-map',
+    entry: './app/index.jsx',
     module: {
         rules: [
           {
             test: /\.(js|jsx)$/,
             exclude: /node_modules/,
             use: ['babel-loader']
+          },{
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
           }
         ]
       },
